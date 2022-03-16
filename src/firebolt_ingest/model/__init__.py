69274744f08e6f86df5775cee30280b00dd1029c
Yury Dzerin <yury@firebolt.io>
2022-03-16 12:48:32 +0100

    add mypy to pre-commit and fix pre-commit warnings

--- src/firebolt_ingest/model/__init__.py
+++ src/firebolt_ingest/model/__init__.py
@@ -1,8 +1,14 @@
-from typing import Any, List
+from typing import Any, Protocol
 
 import yaml
 from pydantic.main import ModelMetaclass
 from yaml import Loader
+
+
+class BaseModelProtocol(Protocol):
+    @classmethod
+    def parse_obj(self, obj: Any) -> Any:
+        ...
 
 
 class YamlModelMixin(metaclass=ModelMetaclass):
@@ -11,6 +17,6 @@ class YamlModelMixin(metaclass=ModelMetaclass):
     """
 
     @classmethod
-    def parse_yaml(cls, yaml_obj: Any):
+    def parse_yaml(cls: BaseModelProtocol, yaml_obj: Any):
         obj = yaml.load(yaml_obj, Loader=Loader)
         return cls.parse_obj(obj)
