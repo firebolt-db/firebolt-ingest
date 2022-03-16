655dccdfd6ef0cc4444451fda2ec43a1013997f7
Yury Dzerin <97026256+yuryfirebolt@users.noreply.github.com>
2022-03-16 12:10:35 +0100

    fix: Fir 12143 fix unit tests in the ci (#1)
    * Move unit tests to unit folder
    
    * fix: add missing pytest-cov dependency
    
    * fix: move the resource manager out of global context

--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -1,6 +1,12 @@
+from unittest.mock import MagicMock
+
 import pytest
 
-from firebolt_ingest import db
+from firebolt_ingest.db import (
+    get_or_create_database,
+    get_or_create_engine,
+    set_up_connection,
+)
 
 
 @pytest.fixture
@@ -23,18 +29,24 @@ def engine_name():
     return "test_engine_name"
 
 
-def test_get_or_create_database(database_name, databases):
-    db.get_or_create_database(database_name)
-    databases.get_by_name.assert_called_with(name=database_name)
+def test_get_or_create_database(mocker, database_name):
+    rm = MagicMock()
+
+    get_or_create_database(rm, database_name)
+    rm.databases.get_by_name.assert_called_with(name=database_name)
 
 
-def test_get_or_create_engine(engine_name, engines):
-    db.get_or_create_engine(engine_name)
-    engines.get_by_name.assert_called_with(name=engine_name)
+def test_get_or_create_engine(engine_name):
+    rm = MagicMock()
+
+    get_or_create_engine(rm, engine_name)
+    rm.engines.get_by_name.assert_called_with(name=engine_name)
 
 
-def test_set_up_connection(mocker, database_name, engine_name, engines, databases):
+def test_set_up_connection(mocker, database_name, engine_name):
     # mock
+    rm = MagicMock()
+
     get_or_create_database = mocker.patch("firebolt_ingest.db.get_or_create_database")
     database = get_or_create_database.return_value
 
@@ -42,7 +54,7 @@ def test_set_up_connection(mocker, database_name, engine_name, engines, database
     engine = get_or_create_engine.return_value
 
     # call
-    db.set_up_connection(database_name=database_name, engine_name=engine_name)
+    set_up_connection(rm, database_name=database_name, engine_name=engine_name)
 
     # assert
     database.attach_to_engine.assert_called_once_with(
