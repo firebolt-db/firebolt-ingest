49b766c6a5442e0a650d453fc826ab3161860fc5
Yury Dzerin <yury@firebolt.io>
2022-03-28 12:03:19 +0300

    feat: move aws_setting out from constructor to the method signature

--- tests/unit/service/test_table_service.py
+++ tests/unit/service/test_table_service.py
@@ -18,8 +18,8 @@ def test_create_external_table_happy_path(
     cursor_mock = MagicMock()
     connection.cursor.return_value = cursor_mock
 
-    ts = TableService(connection, mock_aws_settings)
-    ts.create_external_table(mock_table)
+    ts = TableService(connection)
+    ts.create_external_table(mock_table, mock_aws_settings)
 
     cursor_mock.execute.assert_called_once_with(
         "CREATE EXTERNAL TABLE IF NOT EXISTS table_name "
@@ -32,13 +32,13 @@ def test_create_external_table_happy_path(
     )
 
 
-def test_insert_full_overwrite(mock_aws_settings: AWSSettings, mock_table: Table):
+def test_insert_full_overwrite(mock_table: Table):
     connection = MagicMock()
     cursor_mock = MagicMock()
     cursor_mock.execute.return_value = 0
     connection.cursor.return_value = cursor_mock
 
-    ts = TableService(connection, mock_aws_settings)
+    ts = TableService(connection)
     ts.create_internal_table = MagicMock()
     ts.insert_full_overwrite(
         internal_table=mock_table,
